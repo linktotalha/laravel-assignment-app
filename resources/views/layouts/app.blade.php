b64a2f30aed81d0780ee303bae70bb83ab24cecc
linktotalha <talhajavaid910@gmail.com>
2023-03-10 14:20:02 +0500

    categories done crud

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,7 @@
                                     </a>
                                 </li><!-- .nk-menu-item -->
                                 <li class="nk-menu-item">
-                                    <a href="{{url('projects/create')}}" class="nk-menu-link">
+                                    <a href="{{url('products/create')}}" class="nk-menu-link">
                                         <span class="nk-menu-icon"><em class="icon ni ni-bitcoin-cash"></em></span>
                                         <span class="nk-menu-text">Products</span>
                                     </a>
