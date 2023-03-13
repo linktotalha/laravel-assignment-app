b3e6594a4f5ab41b3389a3dd53f9b9df902a2af2
linktotalha <talhajavaid910@gmail.com>
2023-03-13 18:02:21 +0500

    add sanctum

--- resources/views/regform.blade.php
+++
@@ -1,7 +0,0 @@
-<form action="{{url('regform')}}" method="post">
-    @csrf
-    <input type="text" name="name">
-    <input type="text" name="email">
-    <input type="text" name="password">
-    <input type="submit" value="ok">
-</form>
